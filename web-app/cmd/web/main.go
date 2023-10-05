e5507b5490c65e811eb62306a8fe7379592a5b8e
Nick Fang <nick.fang@huvrdata.com>
2023-10-05 14:28:07 -0500

    Adding app configuration.

--- web-app/cmd/web/main.go
+++ web-app/cmd/web/main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"web-app/pkg/config"
 	"web-app/pkg/handlers"
+	"web-app/pkg/render"
 )
 
 const portNumber = ":8080"
 
 // main is the main application function
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	var app config.AppConfig
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache")
+	}
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", repo.Home)
+	http.HandleFunc("/about", repo.About)
 	http.HandleFunc("/favicon.ico", handlers.Favicon)
 
 	fmt.Printf("Starting application on port %s\n", portNumber)
--- web-app/pkg/render/render.go
+++ web-app/pkg/render/render.go
@@ -2,40 +2,47 @@ package render
 
 import (
 	"bytes"
+	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
-	"text/template"
+	"web-app/pkg/config"
 )
 
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	// create template cache
-	tc, err := createTemplateCache()
-	if err != nil {
-		log.Fatal(err)
+	var tc map[string]*template.Template
+	if app.UseCache {
+		// get template cache from app config
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
 	}
 
 	// get requested template from cache
 	t, ok := tc[tmpl]
 	if !ok {
-		log.Fatal(err)
+		log.Fatal("Could not get template from template cache.")
 	}
 
 	buf := new(bytes.Buffer)
 
-	err = t.Execute(buf, nil)
-	if err != nil {
-		log.Println(err)
-	}
+	_ = t.Execute(buf, nil)
 
 	// render the template
-	_, err = buf.WriteTo(w)
+	_, err := buf.WriteTo(w)
 	if err != nil {
 		log.Println("Error writing template to browser", err)
 	}
 }
 
-func createTemplateCache() (map[string]*template.Template, error) {
+func CreateTemplateCache() (map[string]*template.Template, error) {
 	// cache := make(map[string]*template.Template)
 	cache := map[string]*template.Template{}
 
