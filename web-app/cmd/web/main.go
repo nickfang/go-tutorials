b548c946696492b835e860463821f6608882203b
Nick Fang <nick.fang@huvrdata.com>
2023-10-05 22:41:52 -0500

    Setting up sessions.

--- web-app/cmd/web/main.go
+++ web-app/cmd/web/main.go
@@ -4,22 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 	"web-app/pkg/config"
 	"web-app/pkg/handlers"
 	"web-app/pkg/render"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main application function
 func main() {
-	var app config.AppConfig
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour        // 24 hours in seconds
+	session.Cookie.Persist = true            // persist session across browser restarts
+	session.Cookie.Secure = app.InProduction // set to true in production
+	session.Cookie.SameSite = http.SameSiteLaxMode
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("Cannot create template cache")
 	}
 	app.TemplateCache = tc
 	app.UseCache = false
+	app.Session = session
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
--- web-app/cmd/web/routes.go
+++ web-app/cmd/web/routes.go
@@ -12,9 +12,10 @@ import (
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
+	mux.Use(SessionLoad)
 	mux.Use(middleware.Recoverer)
-	mux.Use(WriteToConsole)
 	mux.Use(NoSurf)
+	mux.Use(WriteToConsole)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
