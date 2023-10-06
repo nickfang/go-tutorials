de6a77e181ed4e5182641cf83d50c5986e4c0e45
Nick Fang <nick.fang@huvrdata.com>
2023-10-06 15:26:38 -0500

    Grabbing html code since I'm skipping this part.

--- bookings/cmd/web/routes.go
+++ bookings/cmd/web/routes.go
@@ -21,5 +21,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/about", handlers.Repo.About)
 	mux.Get("/favicon.ico", handlers.Repo.Favicon)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
