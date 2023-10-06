3decdb05361058b70cdb118067b691af4c059f6b
Nick Fang <nick.fang@huvrdata.com>
2023-10-05 19:26:02 -0500

    Refactoring to use chi instead of pat.

--- web-app/cmd/web/routes.go
+++ web-app/cmd/web/routes.go
@@ -1,18 +1,28 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
 	"net/http"
 	"web-app/pkg/config"
 	"web-app/pkg/handlers"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	// mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-	mux.Get("/favicon.ico", http.HandlerFunc(handlers.Repo.Favicon))
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux.Get("/favicon.ico", http.HandlerFunc(handlers.Repo.Favicon))
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/favicon.ico", handlers.Repo.Favicon)
 
 	return mux
 }
