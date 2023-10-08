a64c722a99b675e1169912092e66834b661cd4a0
Nick Fang <nick.fang@huvrdata.com>
2023-10-07 19:33:45 -0500

    Showing error for a field.

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -77,7 +77,32 @@ func (m *Repository) MakeReservation(w http.ResponseWriter, r *http.Request) {
 
 // PostMakeReservation is the Make Reservation page handler
 func (m *Repository) PostMakeReservation(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
+	reservation := models.Reservation{
+		FirstName: r.Form.Get("first_name"),
+		LastName:  r.Form.Get("last_name"),
+		Email:     r.Form.Get("email"),
+		Phone:     r.Form.Get("phone"),
+	}
+
+	form := forms.New(r.PostForm)
+
+	form.Has("first_name", r)
+
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+
+		render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+			Form: form,
+			Data: data,
+		})
+	}
 }
 
 // SearchAvailability is the Search Availability page handler
