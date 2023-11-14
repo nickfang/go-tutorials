4555acca42fc77a4c73be1a06753947fd7ad3187
Nick Fang <fang.nicholas@gmail.com>
2023-11-14 09:34:39 -0600

    Adding SearchAvailabilityForAllRooms to postgres repository.

--- bookings/internal/repository/repository.go
+++ bookings/internal/repository/repository.go
@@ -10,5 +10,6 @@ type DatabaseRepo interface {
 	AllUsers() bool
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(res models.RoomRestriction) error
-	SearchAvailabilityByDates(roomID int, start, end time.Time) (bool, error)
+	SearchAvailabilityByDatesByRoomID(roomID int, start, end time.Time) (bool, error)
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 }
