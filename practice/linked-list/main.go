b01358fc905f690bf246ad2236f3c1f8799c0de5
Nick Fang <fang.nicholas@gmail.com>
2024-03-08 00:21:08 -0600

    refactoring.

--- practice/linked-list/main.go
+++ practice/linked-list/main.go
@@ -12,34 +12,62 @@ type LinkedList struct {
 	Head *Item
 }
 
-func createListItem(value string) Item {
-	item := Item{value, nil, nil}
-	return item
+func (i *Item) NewItem(value string) *Item {
+	i.Value = value
+	i.Next = nil
+	i.Prev = nil
+	return i
 }
 
-func insertListItem(item *Item, location *Item) {
-	if (item == nil || location == nil) {
+func (i *Item) InsertItem(newItem *Item) {
+	if (i == nil || newItem == nil) {
 		return
 	}
-	temp := location.Next
-	location.Next = item
-	item.Next = temp
+	newItem.Next = i.Next
+	newItem.Prev = i
+	if i.Next != nil {
+		i.Next.Prev = newItem
+	}
+	i.Next = newItem
 }
 
+func (ll *LinkedList) InsertAtBeginning(newItem *Item) {
+	newItem.Next = ll.Head
+	if ll.Head != nil {
+		ll.Head.Prev = newItem
+	}
+	ll.Head = newItem
+}
 
+func showList(item *Item) {
+	if item == nil { return }
+	fmt.Println(item.Value)
+	showList(item.Next)
+}
+
+func (ll *LinkedList) showList() {
+	item := ll.Head
+	for item != nil {
+		fmt.Println(item.Value)
+		item = item.Next
+	}
+}
 
 func showListItem(item *Item) {
 	fmt.Println(item.Value, item.Next, item.Prev)
 }
 
 func main() {
-	head := createListItem("head")
-	item1 := createListItem("1")
-	insertListItem(&item1, &head)
-	item2 := createListItem("2")
-	insertListItem(&item2, &item1)
+	list := LinkedList{}
+	item1 := Item{Value: "1"}
+	item2 := Item{Value: "2"}
+	item3 := Item{Value: "3"}
+	list.InsertAtBeginning(&item2)
+	list.InsertAtBeginning(&item1)
+	item2.InsertItem(&item3)
 
-	showListItem(&head)
+	showListItem(list.Head)
 	showListItem(&item1)
 	showListItem(&item2)
+	list.showList()
 }
