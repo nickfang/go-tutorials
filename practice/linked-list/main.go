df589cc2396f835e7ca83ef1aa673b8a255cdad8
Nick Fang <fang.nicholas@gmail.com>
2024-03-09 00:55:07 -0600

    Implemented generics and finding a value by index and index by value.

--- practice/linked-list/main.go
+++ practice/linked-list/main.go
@@ -2,25 +2,25 @@ package main
 
 import "fmt"
 
-type Item struct {
-	Value string
-	Next *Item
-	Prev *Item
+type Item[T string|int] struct {
+	Value T
+	Next *Item[T]
+	Prev *Item[T]
 }
 
-type LinkedList struct {
-	Head *Item
-	Tail *Item
+type LinkedList[T string|int] struct {
+	Head *Item[T]
+	Tail *Item[T]
 }
 
-func (i *Item) NewItem(value string) *Item {
+func (i *Item[T]) NewItem(value T) *Item[T] {
 	i.Value = value
 	i.Next = nil
 	i.Prev = nil
 	return i
 }
 
-func (i *Item) InsertItem(newItem *Item) {
+func (i *Item[T]) InsertItem(newItem *Item[T]) {
 	if (i == nil || newItem == nil) {
 		return
 	}
@@ -32,16 +32,14 @@ func (i *Item) InsertItem(newItem *Item) {
 	i.Next = newItem
 }
 
-func showListItem(item *Item) {
-	if item == nil { return }
-	fmt.Println(item.Value)
-	showListItem(item.Next)
+func (i *Item[T]) showListItem() {
+	if i == nil { return }
+	fmt.Println(i.Value)
+	i.Next.showListItem()
 }
 
 
-
-
-func (ll *LinkedList) CheckHeadAndTail() error{
+func (ll *LinkedList[T]) CheckHeadAndTail() error{
 	if (ll.Head == nil && ll.Tail != nil) || (ll.Head != nil && ll.Tail == nil) {
 		error := fmt.Errorf("the head (%p) and tail (%p) must both have a value or both be nil", ll.Head, ll.Tail)
 		return error
@@ -49,7 +47,12 @@ func (ll *LinkedList) CheckHeadAndTail() error{
 	return nil
 }
 
-func (ll *LinkedList) AddToBeginning(newItem *Item) {
+func (ll *LinkedList[T]) IsEmpty() bool{
+	return ll.Head == nil && ll.Tail == nil
+}
+
+
+func (ll *LinkedList[T]) AddToBeginning(newItem *Item[T]) {
 	error := ll.CheckHeadAndTail()
 	if (error != nil) {
 		return
@@ -63,23 +66,28 @@ func (ll *LinkedList) AddToBeginning(newItem *Item) {
 	ll.Head = newItem
 }
 
-func (ll *LinkedList) AddToEnd(newItem * Item) {
+func (ll *LinkedList[T]) AddToEnd(newItem *Item[T]) {
 	error := ll.CheckHeadAndTail()
-	if (error != nil) {
+	if error != nil {
 		return
 	}
-	if ll.Tail == nil {
+	if ll.IsEmpty() {
 		ll.Head = newItem
 		ll.Tail = newItem
+		return
 	}
 	newItem.Prev = ll.Tail
 	ll.Tail.Next = newItem
 	ll.Tail = newItem
 }
 
-func (ll *LinkedList) GetLength() int {
+func (ll *LinkedList[T]) GetLength() int {
+	error := ll.CheckHeadAndTail()
+	if error != nil {
+		return 0
+	}
 	length := 0
-	if ll.Head == nil {
+	if ll.IsEmpty() {
 		return length
 	}
 	index := ll.Head
@@ -90,17 +98,49 @@ func (ll *LinkedList) GetLength() int {
 	return length
 }
 
-func (ll *LinkedList) GetIndexValue(index int) string{
-
+func (ll *LinkedList[T]) GetValueOfIndex(index int) T {
+	var defaultValue T
+	error := ll.CheckHeadAndTail()
+	if error != nil {
+		return defaultValue
+	}
+	if ll.IsEmpty() {
+		fmt.Printf("Empty list.\n")
+		return defaultValue
+	}
+	i := 0
+	item := ll.Head
+	for item != nil {
+		if i == index {
+			return item.Value
+		}
+		i = i+1
+		item = item.Next
+	}
+	fmt.Printf("Index is outside of the list.")
+	return defaultValue
 }
 
-func (ll *LinkedList) GetValueIndex(value string) int {
-
+func (ll *LinkedList[T]) GetIndexOfValue(value T) int {
+	var defaultValue int
+	error := ll.CheckHeadAndTail()
+	if error != nil {
+		return defaultValue
+	}
+	if ll.IsEmpty() {
+		fmt.Printf("Empty list.\n")
+		return defaultValue
+	}
+	i := 0
+	item := ll.Head
+	for item.Value != value {
+		i = i + 1
+		item = item.Next
+	}
+	return i
 }
 
-
-
-func (ll *LinkedList) showList() {
+func (ll *LinkedList[T]) showList() {
 	item := ll.Head
 	for item != nil {
 		fmt.Println(item.Value)
@@ -109,10 +149,12 @@ func (ll *LinkedList) showList() {
 }
 
 func main() {
-	list := LinkedList{}
-	item1 := Item{Value: "1"}
-	item2 := Item{Value: "2"}
-	item3 := Item{Value: "3"}
+	list := LinkedList[string]{}
+	item1 := Item[string]{Value: "1"}
+	item2 := Item[string]{Value: "2"}
+	item3 := Item[string]{Value: "3"}
+	emptyValue := list.GetValueOfIndex(1)
+	fmt.Printf("%s", emptyValue)
 	list.AddToBeginning(&item2)
 	list.AddToBeginning(&item1)
 	item2.InsertItem(&item3)
@@ -122,4 +164,14 @@ func main() {
 	// showListItem(&item2)
 	list.showList()
 	fmt.Printf("length %d\n", list.GetLength())
+	value4 := list.GetValueOfIndex(4)
+	fmt.Printf("%s\n", value4)
+	value1 := list.GetValueOfIndex(1)
+	fmt.Printf("%s\n", value1)
+	index1 := list.GetIndexOfValue("1")
+	fmt.Printf("Index of '1': %d\n", index1)
+	index2 := list.GetIndexOfValue("2")
+	fmt.Printf("Index of '2': %d\n", index2)
+	index3 := list.GetIndexOfValue("3")
+	fmt.Printf("Index of '3': %d\n", index3)
 }
