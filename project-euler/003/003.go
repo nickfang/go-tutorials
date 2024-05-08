8ec220ae7ca54e5dd622b0cfa202ada28838acea
Nick Fang <fang.nicholas@gmail.com>
2024-05-08 17:48:42 -0500

    Solution to problem 3.

--- project-euler/003/003.go
+++ project-euler/003/003.go
@@ -1,8 +1,30 @@
 package main
 
-import "fmt"
+// The prime factors of 13195 are 5, 7, 13 and 29
+// What is the largest prime factor of the number 600851475143
+
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-  answer := ""
-  fmt.Println(answer)
+  largestFactor := 1
+  number := 600851475143
+
+  if number % 2 == 0 {
+    number = (number / 2)
+    largestFactor = 2
+  }
+
+  end := int(math.Sqrt(float64(number)))
+
+  for x := 3; x < end; x += 2 {
+    if number % x == 0 {
+      number = number / x
+      largestFactor = x
+    }
+  }
+  fmt.Println(largestFactor)
+  // 6857
 }
