b16acf89d45468e4f24b669610d53bcf64de5d94
Nick Fang <fang.nicholas@gmail.com>
2024-05-13 17:33:07 -0500

    Solved problem 7.

---
+++ project-euler/007/007.go
@@ -0,0 +1,28 @@
+package main
+
+// By listing the first six prime numbers: 2, 3, 5, 7, 11, 13, we can see that the 6th prime is 13.
+// What is the 10001st prime number?
+
+import "fmt"
+
+func main() {
+  answer := 0
+  primes := []int{2,3,5,7,11,13}
+  num := 15
+  for len(primes) < 10001 {
+    isPrime := true
+    for _, prime := range primes {
+      if num % prime == 0 {
+        isPrime = false
+        break
+      }
+    }
+    if isPrime {
+      primes = append(primes, num)
+    }
+    num += 2
+  }
+  answer = primes[10000]
+  fmt.Println(answer)
+  // 104743
+}
