8f47d014a6e437fe857f7a78d1aee2c44472aa82
Nick Fang <fang.nicholas@gmail.com>
2024-06-08 17:06:44 -0500

    updating 006.

--- project-euler/006/006.go
+++ project-euler/006/006.go
@@ -1,19 +1,27 @@
+// The sum of the squares of the first ten natural numbers is 1^2 + 2^2 + ... + 10^2 = 385,
+
+// The square of the sum of the first ten natural numbers is (1 + 2 + ... + 10)^2 = 55^2 = 3025,
+
+// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
 package main
 
 import "fmt"
 
 func main() {
-  answer := 0
-  sumOfSquare := 0
-  squareOfSum := 0
-  for x := 1; x <= 100; x++ {
-    sumOfSquare += x * x
-    squareOfSum += x
-  }
-  squareOfSum *= squareOfSum
-  fmt.Println(sumOfSquare)
-  fmt.Println(squareOfSum)
-  answer = squareOfSum - sumOfSquare
-  fmt.Println(answer)
-  // 25164150
+	answer := 0
+	sumOfSquare := 0
+	squareOfSum := 0
+	for x := 1; x <= 100; x++ {
+		sumOfSquare += x * x
+		squareOfSum += x
+	}
+	squareOfSum *= squareOfSum
+	fmt.Println(sumOfSquare)
+	fmt.Println(squareOfSum)
+	answer = squareOfSum - sumOfSquare
+	fmt.Println(answer)
+	// 25164150
 }
